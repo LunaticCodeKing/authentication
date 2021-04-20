b62e7d596ebef74ce93613b5fff81a57647ea521
LunaticCodeKing <shakthi.praveen1010@gmail.com>
2021-04-20 12:28:22 +0530

    commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://praveen:zzDQ6tdnLKMnJ9xA@cluster0.johzw.mongodb.net/authenticationsdetails?retryWrites=true&w=majority",
+  secretOrKey: "1DbZjeQSI7eAtqhrBGKptrit0TBkPqnQ"
+};
